7cc253ecec85dd4f339c53dfa717359b6ff394bf
OrianeORDS <oriane.royon@gmail.com>
2024-12-25 13:07:01 +0100

    adjusting icons

--- components/Section.js
+++ components/Section.js
@@ -1,5 +1,5 @@
 import SectionButton from "@/components/SectionButton"; 
-import ArrowListIcon from "./icons/ArrowListIcon";
+import {ListArrow, ReactIcon, PortefolioIcon, BadmintonIcon, ArtIcon} from "./IconsSVG";
 
 
 export default function Section() {
@@ -8,16 +8,16 @@ export default function Section() {
                 quiJeSuis : [ 
                         {text : "Développeuse Full-stack, j’aime être à l’interface entre les exigences du design, et les contraintes du traitement des données, pour offrir une expérience utilisateur fluide et visuellement agréable."},
                         {text: "Le métier de développeuse me passionne pour trois raisons principales :"},
-                        {icon: <ArrowListIcon/>, text:  "L’apprentissage constant qu’il implique,"},
-                        {icon: <ArrowListIcon/>, text:  "La créativité qu’offre le maniement des technologies,"}, 
-                        {icon: <ArrowListIcon/>, text:  "Le caractère concret et mesurable des réalisations,"},     
+                        {icon: <ListArrow/>, text:  "L’apprentissage constant qu’il implique,"},
+                        {icon: <ListArrow/>, text:  "La créativité qu’offre le maniement des technologies,"}, 
+                        {icon: <ListArrow/>, text:  "Le caractère concret et mesurable des réalisations,"},     
                         { text : "Enfin, j'apprécie les collaborations inhérentes au monde de la tech : j’aime travailler en équipe pour concevoir, développer et améliorer des applications web." },
                 ],
                 mesProjets : [
-                        { /* icon: <FaReact />,*/ text: "React based app using a museum API" },
-                        { /* icon: <FaCode />,*/ text: "Portfolio with Next and Tailwind" },
-                        { /*icon: <FaNodeJs />,*/ text: "A blog about learning Node.js" },
-                        { /*icon: <FaCode />,*/  text: "Portfolio with Next and Tailwind" },
+                        { icon: <ReactIcon/>, text: "React based app using a museum API" },
+                        { icon: <PortefolioIcon />, text: "Portfolio with Next and Tailwind" },
+                        { icon: <BadmintonIcon/>, text: "A blog about learning Node.js" },
+                        { icon : <ArtIcon/>, text: "Portfolio with Next and Tailwind" },
                 ],
         };
 
--- components/SectionButton.js
+++ components/SectionButton.js
@@ -9,8 +9,8 @@ export default function SectionButton(props) {
     console.log(props.items)
 
     return (
-        <button onClick={handleToggle} className= {`relative flex flex-col justify-center items-left bg-dark-purple hover:bg-intense-purple
-        ${isOpen ? "w-5/6" : "w-2/3"} boder-box  sm:w-2/5 ${isOpen ? "h-auto" : "h-24"} p-4 rounded-xl shadow-lg `}
+        <button onClick={handleToggle} className= {`relative flex flex-col justify-center items-left bg-dark-purple-90 hover:bg-intense-purple
+        ${isOpen ? "w-5/6" : "w-2/3"} boder-box  sm:w-2/5 ${isOpen ? "h-auto" : "h-24"} p-5 rounded-xl shadow-lg `}
          >
           <h2 className="text-medium-purple text-center uppercase w-full boder-box m-0 font-bold font-oswald text-base">
           {props.title}
@@ -20,7 +20,8 @@ export default function SectionButton(props) {
             <ul className="flex flex-col text-white-green m-0 p-0 text-left font-normal mt-4 ">
               {props.items.map((item, index) => (
                 <li key={index} className="flex items-top gap-2 text-white-green text-left py-1">
-                {item.icon? <span className="fill-medium-purple">{item.icon}</span> : null}
+                {item.icon? <span className="fill-medium-purple h-6 w-6 flex justify-centerc
+                ">{item.icon}</span> : null}
                   <span className="text-xs text-justify">{item.text}</span>
                 </li>
               ))}
