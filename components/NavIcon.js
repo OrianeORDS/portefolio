d3de982b76106b0de7053eda44f8275a4aeaf34f
OrianeORDS <oriane.royon@gmail.com>
2024-12-26 15:05:12 +0100

    cross becomes a minus sign when box is opened

--- components/NavIcon.js
+++ components/NavIcon.js
@@ -5,7 +5,7 @@ export default function NavIcon(props) {
   // icones centrées, plusieurs tailles possibles, et au minimum un container de 40px de large cliquables pour faciliter l'accessibilité
   return (
     <a href={props.goTo} className="w-10 flex justify-center items-center">
-      <div className=" flex justify-center items-center w-6 h-6 sm:w-8 sm:h-8 fill-medium-purple  ">
+      <div className=" flex justify-center items-center w-6 h-6 sm:w-8 sm:h-8 fill-light-purple hover:fill-medium-purple  ">
         {props.icon}
      </div>
   </a>
