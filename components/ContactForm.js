93ded0d1815b5126b0b35c8536a8ad654609112c
OrianeORDS <oriane.royon@gmail.com>
2024-12-27 20:04:36 +0100

    when email send => show message to user instead of form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,6 +9,8 @@ export default function ContactForm() {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   const [status, setStatus] = useState('');
   const [errors, setErrors] = useState({});
+  const [formVisible, setFormVisible] = useState(false);
+
 
 
   function handleChange(event) {
@@ -23,6 +25,7 @@ export default function ContactForm() {
       }
     })
   }
+
   async function handleSubmit(e) {
     e.preventDefault();  
     try {
@@ -36,11 +39,12 @@ export default function ContactForm() {
   
       const result = await res.json();
       if (result.success) {
-        console.log("tout va")
-        setStatus('Votre message a été envoyé avec succès.') ;
+        console.log("Votre message a été envoyé avec succès")
+        setStatus('Votre message a été envoyé avec succès. Une copie vous a été adressée.') ;
         setFormData({ name: "", email: "", message: "" });
+        setFormVisible(false);
       } else {
-        console.log("rien ne va")
+        console.log("Une erreur est survenue lors de l\'envoi.")
         setStatus('Une erreur est survenue lors de l\'envoi.');
       }
     } catch (error) {
@@ -49,6 +53,11 @@ export default function ContactForm() {
     }
   }
 
+  function handleNewForm() {
+    setFormVisible(true); 
+    setStatus("");
+  };
+
 
 
 return (
@@ -56,58 +65,72 @@ return (
   <div className="max-w-lg mx-auto py-5 w-full">
     <h2 className= "text-medium-purple text-left uppercase w-full boder-box py-5 m-0 font-oswald text-lg sm:text-xl ">
     Formulaire de contact </h2>
-    <form className="flex flex-col py-5 gap-4 font-source" onSubmit={handleSubmit} >
-      <FormInput
-        labeltext="Votre email :"
-        htmlFor="email"
-        id="email"
-        name="email"
-        type="email"
-        value={formData.email}
-        required
-        placeholder="Entrez votre email"
-        onInputChange={handleChange}
-        />
-      {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
 
-      <FormInput
-        labeltext="Votre nom"
-        htmlFor="name"
-        id="name"
-        name="name"
-        type="text"
-        value={formData.name}
-        placeholder="Entrez votre nom :"
-        required
-        onInputChange={handleChange}
-        />
-      {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
-      
-      <div> 
-        <Label labeltext="Votre message :"  htmlFor="message" />
-        <textarea
-          name="message"
-          id="message"
-          rows="4"
-          className="mt-1 block w-full box-border rounded-md bg-white text-dark-purple p-2 border border-medium-purple focus:border-intense-purple focus:ring-intense-purple"
-          placeholder="Tapez votre message ici..."
-          value={formData.message}
+    {formVisible ? (
+      <form className="flex flex-col py-5 gap-4 font-source" onSubmit={handleSubmit} >
+        <FormInput
+          labeltext="Votre email :"
+          htmlFor="email"
+          id="email"
+          name="email"
+          type="email"
+          value={formData.email}
           required
-          minLength="10"
-          onChange={(e) => handleChange(e)} 
-        ></textarea>
-        {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
+          placeholder="Entrez votre email"
+          onInputChange={handleChange}
+          />
+        {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
 
-      </div>
+        <FormInput
+          labeltext="Votre nom"
+          htmlFor="name"
+          id="name"
+          name="name"
+          type="text"
+          value={formData.name}
+          placeholder="Entrez votre nom :"
+          required
+          onInputChange={handleChange}
+          />
+        {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
         
+        <div> 
+          <Label labeltext="Votre message :"  htmlFor="message" />
+          <textarea
+            name="message"
+            id="message"
+            rows="4"
+            className="mt-1 block w-full box-border rounded-md bg-white text-dark-purple p-2 border border-medium-purple focus:border-intense-purple focus:ring-intense-purple"
+            placeholder="Tapez votre message ici..."
+            value={formData.message}
+            required
+            minLength="10"
+            onChange={(e) => handleChange(e)} 
+          ></textarea>
+          {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
 
-      <button
-        type="submit"
-        className="bg-intense-green text-white py-2 px-4 rounded-md shadow-md hover:bg-intense-green focus:ring-2 focus:ring-intense-green focus:outline-none"
-      >
-        Envoyer
-      </button>
-    </form>
+        </div>
+          
+
+        <button
+          type="submit"
+          className="bg-intense-green text-white py-2 px-4 rounded-md shadow-md hover:bg-intense-green focus:ring-2 focus:ring-intense-green focus:outline-none"
+        >
+          Envoyer
+        </button>
+      </form> 
+    ) :  (
+      <div className="flex flex-col text-justify gap-5">
+        <p className=" text-sm bg-white-green-25 py-10 px-5 rounded-sm ">{status}</p>
+        <button
+          onClick={handleNewForm}
+          className="bg-intense-green text-white py-2 px-4 rounded-md shadow-md hover:bg-intense-green focus:ring-2 focus:ring-intense-green focus:outline-none"
+        >
+          Remplir un nouveau formulaire
+        </button>
   </div>
+    ) } 
+
+    </div>
 
 )}
