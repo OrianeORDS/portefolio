52e89c8d137d36b89fa719dfe798f90e906764ba
OrianeORDS <oriane.royon@gmail.com>
2025-02-14 09:42:51 +0100

    Display date in French format in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return(
         <footer className="bg-dark-purple-90 text-medium-purple py-5 px-10 flex justify-end text-xs sm:text-base">
-        <p className="m-0">© ORDS 26/12/2024 - Last update : {new Date(lastUpdate).toLocaleDateString()}</p>
+        <p className="m-0">© ORDS 26/12/2024 - Last update : {new Date(lastUpdate).toLocaleDateString('fr-FR')}</p>
         </footer>
 
 
